03ffa3839f7994cea85a850e02305a65df60fe93
Von Dedeles <116178585+Vooonvoon@users.noreply.github.com>
2023-05-22 23:22:33 +0800

    Add files via upload

---
+++ PTscript.js
@@ -0,0 +1,17 @@
+function outer() {
+    let name = "outer";
+    let str = inner();
+    return str;
+    }
+    
+    function inner() {
+    let name = "inner";
+    return "Hello Von!";
+    }
+    
+    console.log("before outer() call");
+    
+    console.log(outer());
+    console.log("after outer() call");
+    
+    
